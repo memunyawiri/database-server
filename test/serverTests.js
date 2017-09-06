e61ac6d2015dffefece2139a347fa9367d55add4
Mutsa <mutsa.evelyn@hotmail.com>
2017-09-06 15:09:42 +0100

    commit for review

--- test/serverTests.js
+++ test/serverTests.js
@@ -18,4 +18,45 @@ describe('server response', function() {
       done();
     });
   });
+
+  it('should return 200', function (done) {
+    var options = {
+      url: 'http://localhost:4000',
+      headers: {
+        'Content-Type': 'text/plain'
+      }
+    };
+    request.get(options, function (err, res, body) {
+      expect(res.statusCode).to.equal(200);
+      expect(res.body).to.equal('something here');
+      done();
+    });
+  });
+
+  it('should emit request body', function (done) {
+    var options = {
+      url: 'http://localhost:8000',
+      headers: {
+        'Content-Type': 'text/plain'
+      },
+      body: 'request received'
+    };
+    var eventFired = false;
+
+    request.get(options, function (err, res, body) {});
+
+    server.on('success', function (data) {
+      eventFired = true;
+      expect(data).to.equal('request received');
+    });
+
+    setTimeout( function () {
+      expect(eventFired).to.equal(true);
+      done();
+    }, 10);
+  });
+  it('should set params', function(done) {
+    .get('/')
+    .query({name: 'mutsa'})
+  })
 });
